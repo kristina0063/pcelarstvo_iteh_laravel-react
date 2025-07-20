0ea71c05326d20143ae46d97c4abc6f3b945f887
kristina0063 <ivankovickristina98@gmail.com>
2025-07-20 14:45:49 +0200

    Dodat je guest layout

---
+++ src/components/layouts/GuestLayout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { useStateContext } from '../../context/context'
+import { Navigate, Outlet } from 'react-router-dom';
+
+export default function GuestLayout () {
+    const {token} = useStateContext();
+    console.log(token);
+    return !token ? <Outlet/>: <Navigate to="/aktivnosti" replace/>;
+}
