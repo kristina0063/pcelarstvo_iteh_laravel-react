ce89bbfe30d075a4cdd7bd393a31cae6607d0260
kristina0063 <ivankovickristina98@gmail.com>
2025-07-20 14:44:33 +0200

    Dodat je default layout

---
+++ src/components/layouts/DefaultLayout.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+import { Navigate, Outlet } from 'react-router-dom';
+import { useStateContext } from '../../context/context';
+
+export default function DefaultLayout() {
+  const {token} = useStateContext();
+  console.log(token);
+  return token ? <Outlet /> : <Navigate to="/login" replace/>;
+}
